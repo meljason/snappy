73caf4d3e251cc8de02bdff618b56963e6e2517a
unknown <jasoncwy@hotmail.com>
2020-11-16 10:41:14 -0500

    First commit video social media

---
+++ src/Component/Video.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../video.css';
+
+function Video() {
+    return (
+        <div className="video">
+            <video className="video__player" src="https://v77.tiktokcdn.com/a197dfab135f4fc3392732ceb534847d/5fb2f12f/video/tos/alisg/tos-alisg-pve-0037c001/19a141e49c6040e1bd9546efffa982a5/?a=1233&br=1250&bt=625&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020111615374001019021820346018970&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajl1eDN3OGc5eDMzMzczM0ApPGY3NjVkOjw8N2VoNGU1N2dzZmZeNnBxbl5fLS1iMTRzczYwMmA0X2JjMDBiLWM1YjY6Yw%3D%3D&vl=&vr="></video>
+        </div>
+    )
+}
+
+export default Video
